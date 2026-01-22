ad5119391f671405830de0bb432b775e57f035bb
Gauthier Jakubowicz <gauthier.jakubowicz@etu.univ-smb.fr>
2026-01-22 09:22:45 +0100

    Codage GetAll

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,61 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        int id;
+        string nomdevise;
+        double taux;
+
+        public Devise()
+        {
+        }
+
+        public Devise(int id, string nomdevise, double taux)
+        {
+            this.Id = id;
+            this.Nomdevise = nomdevise;
+            this.Taux = taux;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public string Nomdevise
+        {
+            get
+            {
+                return this.nomdevise;
+            }
+
+            set
+            {
+                this.nomdevise = value;
+            }
+        }
+
+        public double Taux
+        {
+            get
+            {
+                return this.taux;
+            }
+
+            set
+            {
+                this.taux = value;
+            }
+        }
+
+        // Je commente pour le commit
+    }
+}
